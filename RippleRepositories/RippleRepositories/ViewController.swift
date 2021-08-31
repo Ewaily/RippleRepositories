d818f6bfb8b055f5d48cf670c6e1554659161fea
Muhammad Ewaily <muhammad.ewaily@inovaeg.com>
2021-08-31 14:24:18 +0200

    [update] project name

---
+++ RippleRepositories/RippleRepositories/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RippleRepositories
+//
+//  Created by Muhammad Ewaily on 31/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
